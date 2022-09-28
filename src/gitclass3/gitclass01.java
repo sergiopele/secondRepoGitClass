4486ccc99a2e74a7d18c07fc26357e42e9435fa0
sergiopele <sergiypele@gmail.com>
2022-09-27 20:05:16 -0400

    thirdVersion

--- src/gitclass3/gitclass01.java
+++ src/gitclass3/gitclass01.java
@@ -5,6 +5,7 @@ public class gitclass01 {
 	public static void main(String[] args) {
 		System.out.println("It's git class");
 		System.out.println("Changes");
+		System.out.println("changes No2");
 
 	}
 
